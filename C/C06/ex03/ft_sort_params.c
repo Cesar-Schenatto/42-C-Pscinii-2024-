65402eb98c48d530ebaf6af5aa4debad7a43d38c
Cesar-Schenatto <158102230+Cesar-Schenatto@users.noreply.github.com>
2024-08-21 18:30:37 +0100

    Add files via upload

--- C/C06/ex03/ft_sort_params.c
+++ C/C06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: ceaugust <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 19:12:05 by ceaugust          #+#    #+#             */
-/*   Updated: 2024/08/20 20:11:32 by ceaugust         ###   ########.fr       */
+/*   Updated: 2024/08/21 18:29:50 by ceaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,54 @@ void ft_putstr(char *str)
 {
 	while (*str)
 	{
-		write(1, str, 1)
+		write(1, str, 1);
 		str++;
 	}
 }
 
+void ft_swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int ft_strcmp(char *s1, char *s2)
+{
+	int c;
+
+	c = 0;
+	while (s1[c] &&  s2[c])
+	{
+		if (s1[c] != s2[c])
+			return (s1[c] - s2[c]);
+		c++;
+	}
+	return (0);
+}
+
+int main (int ac, char **av)
+{
+	int i;
+	int j;
+	int an[ac];
+	
+	i = 1;
+	while(i < ac )
+	{
+		an[i] = i;
+		j = i;
+		while (j < ac )
+		{
+			if (ft_strcmp(av[an[i]], av[an[j]]) > 0)
+				ft_swap(&an[i], &an[j]);
+			j++;
+		}
+		ft_putstr(av[an[i++]]);
+	}
+	
+	return (0);
+}
+// not finished!
