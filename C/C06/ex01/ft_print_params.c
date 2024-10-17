3ecb6aa526c808bf433ec327a6d8ed6c7cca18a0
Cesar-Schenatto <158102230+Cesar-Schenatto@users.noreply.github.com>
2024-10-17 16:58:43 +0100

    Updated C06

--- C/C06/ex01/ft_print_params.c
+++ C/C06/ex01/ft_print_params.c
@@ -6,29 +6,28 @@
 /*   By: ceaugust <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 18:58:50 by ceaugust          #+#    #+#             */
-/*   Updated: 2024/08/20 15:49:49 by ceaugust         ###   ########.fr       */
+/*   Updated: 2024/08/21 20:33:42 by ceaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_params(char *str)
+void	ft_print_params(char *str)
 {
 	int	i;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
 		write(1, &str[i], 1);
 		i++;
 	}
 }
 
-int main (int ac, char **av)
+int	main(int ac, char **av)
 {
-	int c;
+	int	c;
 
-	
 	c = 1;
 	while (c < ac)
 	{
--- C/C06/ex03/ft_sort_params.c
+++ C/C06/ex03/ft_sort_params.c
@@ -6,13 +6,13 @@
 /*   By: ceaugust <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 19:12:05 by ceaugust          #+#    #+#             */
-/*   Updated: 2024/08/21 19:07:00 by ceaugust         ###   ########.fr       */
+/*   Updated: 2024/08/21 20:31:11 by ceaugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h> 
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
 	while (*str)
 	{
@@ -22,21 +22,21 @@ void ft_putstr(char *str)
 	write(1, "\n", 1);
 }
 
-void ft_swap(int *a, int *b)
+void	ft_swap(int *a, int *b)
 {
-	int temp;
+	int	temp;
 
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-int ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
-	int c;
+	int	c;
 
 	c = 0;
-	while (s1[c] &&  s2[c])
+	while (s1[c] && s2[c])
 	{
 		if (s1[c] != s2[c])
 			return (s1[c] - s2[c]);
@@ -45,33 +45,30 @@ int ft_strcmp(char *s1, char *s2)
 	return (0);
 }
 
-int main (int ac, char **av)
+int	main(int ac, char **av)
 {
-	int i;
-	int j;
-	int an[ac];
-	int antiBO;
-	
-	
+	int	i;
+	int	j;
+	int	an[16384];
+	int	antibo;
+
 	i = 1;
-	antiBO = 1;
-	while(antiBO < ac)
+	antibo = 1;
+	while (antibo < ac)
 	{
-		an[antiBO] = antiBO;
-		antiBO++;
+		an[antibo] = antibo;
+		antibo++;
 	}
-	while(i < ac )
+	while (i < ac)
 	{
-		j = i;
-		while (j < ac )
+		j = i + 1;
+		while (j < ac)
 		{
-			if (ft_strcmp(av[i], av[j]) > 0)
+			if (ft_strcmp(av[an[i]], av[j]) > 0)
 				ft_swap(&an[i], &an[j]);
 			j++;
 		}
 		ft_putstr(av[an[i++]]);
 	}
-	
 	return (0);
 }
-// not finished!
